61eeffa66babaa372ccc9ff5ce87293b02dbbee6
YinX518 <930518ivy2004@gmail.com>
2024-11-24 12:54:17 +0800

    底換成白色

--- checkers.py
+++ checkers.py
@@ -28,15 +28,15 @@ def main(gamemode):
 
     while True:
         # GUI
-        DISPLAYSURF.fill((0, 0, 0))
+        DISPLAYSURF.fill((255, 255, 255))
         game_control.draw_screen(DISPLAYSURF)
 
         turn_display_text = "White's turn" if game_control.get_turn() == "W" else "Black's turn"
-        DISPLAYSURF.blit(main_font.render(turn_display_text, True, (255, 255, 255)), turn_rect)
+        DISPLAYSURF.blit(main_font.render(turn_display_text, True, (0, 0, 0)), turn_rect)
 
         if game_control.get_winner() is not None:
             winner_display_text = "White wins!" if game_control.get_winner() == "W" else "Black wins!"
-            DISPLAYSURF.blit(main_font.render(winner_display_text, True, (255, 255, 255)), winner_rect)
+            DISPLAYSURF.blit(main_font.render(winner_display_text, True, (0, 0, 0)), winner_rect)
 
         # Event handling
         for event in pg.event.get():
